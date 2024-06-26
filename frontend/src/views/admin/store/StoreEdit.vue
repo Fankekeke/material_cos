edcdc45ca59dbd7ecda84416405f07b86901fa8c
FanK <1258846421@qq.com>
2024-06-26 13:59:59 +0800

    fix - 库房，计量单位

---
+++ frontend/src/views/admin/store/StoreEdit.vue
@@ -0,0 +1,177 @@
+<template>
+  <a-modal v-model="show" title="修改库房" @cancel="onClose" :width="800">
+    <template slot="footer">
+      <a-button key="back" @click="onClose">
+        取消
+      </a-button>
+      <a-button key="submit" type="primary" :loading="loading" @click="handleSubmit">
+        修改
+      </a-button>
+    </template>
+    <a-form :form="form" layout="vertical">
+      <a-row :gutter="20">
+        <a-col :span="12">
+          <a-form-item label='库房名称' v-bind="formItemLayout">
+            <a-input v-decorator="[
+            'name',
+            { rules: [{ required: true, message: '请输入名称!' }] }
+            ]"/>
+          </a-form-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-item label='负责人' v-bind="formItemLayout">
+            <a-input v-decorator="[
+            'principal',
+            { rules: [{ required: true, message: '请输入负责人!' }] }
+            ]"/>
+          </a-form-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-item label='库房地址' v-bind="formItemLayout">
+            <a-input v-decorator="[
+            'address',
+            { rules: [{ required: true, message: '请输入库房地址!' }] }
+            ]"/>
+          </a-form-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-item label='联系方式' v-bind="formItemLayout">
+            <a-input v-decorator="[
+            'contact',
+            { rules: [{ required: true, message: '请输入联系方式!' }] }
+            ]"/>
+          </a-form-item>
+        </a-col>
+        <a-col :span="24">
+          <a-form-item label='备注' v-bind="formItemLayout">
+            <a-textarea :rows="6" v-decorator="[
+            'content',
+             { rules: [{ required: true, message: '请输入!' }] }
+            ]"/>
+          </a-form-item>
+        </a-col>
+      </a-row>
+    </a-form>
+  </a-modal>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+function getBase64 (file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = error => reject(error)
+  })
+}
+const formItemLayout = {
+  labelCol: { span: 24 },
+  wrapperCol: { span: 24 }
+}
+export default {
+  name: 'storeEdit',
+  props: {
+    storeEditVisiable: {
+      default: false
+    }
+  },
+  computed: {
+    ...mapState({
+      currentUser: state => state.account.user
+    }),
+    show: {
+      get: function () {
+        return this.storeEditVisiable
+      },
+      set: function () {
+      }
+    }
+  },
+  data () {
+    return {
+      rowId: null,
+      formItemLayout,
+      form: this.$form.createForm(this),
+      loading: false,
+      fileList: [],
+      previewVisible: false,
+      previewImage: ''
+    }
+  },
+  methods: {
+    handleCancel () {
+      this.previewVisible = false
+    },
+    async handlePreview (file) {
+      if (!file.url && !file.preview) {
+        file.preview = await getBase64(file.originFileObj)
+      }
+      this.previewImage = file.url || file.preview
+      this.previewVisible = true
+    },
+    picHandleChange ({ fileList }) {
+      this.fileList = fileList
+    },
+    imagesInit (images) {
+      if (images !== null && images !== '') {
+        let imageList = []
+        images.split(',').forEach((image, index) => {
+          imageList.push({uid: index, name: image, status: 'done', url: 'http://127.0.0.1:9527/imagesWeb/' + image})
+        })
+        this.fileList = imageList
+      }
+    },
+    setFormValues ({...store}) {
+      this.rowId = store.id
+      let fields = ['name', 'principal', 'contact', 'address', 'content']
+      let obj = {}
+      Object.keys(store).forEach((key) => {
+        if (key === 'images') {
+          this.fileList = []
+          this.imagesInit(store['images'])
+        }
+        if (fields.indexOf(key) !== -1) {
+          this.form.getFieldDecorator(key)
+          obj[key] = store[key]
+        }
+      })
+      this.form.setFieldsValue(obj)
+    },
+    reset () {
+      this.loading = false
+      this.form.resetFields()
+    },
+    onClose () {
+      this.reset()
+      this.$emit('close')
+    },
+    handleSubmit () {
+      // 获取图片List
+      let images = []
+      this.fileList.forEach(image => {
+        images.push(image.name)
+      })
+      this.form.validateFields((err, values) => {
+        values.id = this.rowId
+        values.images = images.length > 0 ? images.join(',') : null
+        if (!err) {
+          this.loading = true
+          this.$put('/cos/store-info', {
+            ...values
+          }).then((r) => {
+            this.reset()
+            this.$emit('success')
+          }).catch(() => {
+            this.loading = false
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/admin/unit/UnitEdit.vue
@@ -0,0 +1,145 @@
+<template>
+  <a-modal v-model="show" title="修改计量单位" @cancel="onClose" :width="400">
+    <template slot="footer">
+      <a-button key="back" @click="onClose">
+        取消
+      </a-button>
+      <a-button key="submit" type="primary" :loading="loading" @click="handleSubmit">
+        修改
+      </a-button>
+    </template>
+    <a-form :form="form" layout="vertical">
+      <a-row :gutter="20">
+        <a-col :span="24">
+          <a-form-item label='计量单位名称' v-bind="formItemLayout">
+            <a-input v-decorator="[
+            'name',
+            { rules: [{ required: true, message: '请输入名称!' }] }
+            ]"/>
+          </a-form-item>
+        </a-col>
+      </a-row>
+    </a-form>
+  </a-modal>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+function getBase64 (file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = error => reject(error)
+  })
+}
+const formItemLayout = {
+  labelCol: { span: 24 },
+  wrapperCol: { span: 24 }
+}
+export default {
+  name: 'unitEdit',
+  props: {
+    unitEditVisiable: {
+      default: false
+    }
+  },
+  computed: {
+    ...mapState({
+      currentUser: state => state.account.user
+    }),
+    show: {
+      get: function () {
+        return this.unitEditVisiable
+      },
+      set: function () {
+      }
+    }
+  },
+  data () {
+    return {
+      rowId: null,
+      formItemLayout,
+      form: this.$form.createForm(this),
+      loading: false,
+      fileList: [],
+      previewVisible: false,
+      previewImage: ''
+    }
+  },
+  methods: {
+    handleCancel () {
+      this.previewVisible = false
+    },
+    async handlePreview (file) {
+      if (!file.url && !file.preview) {
+        file.preview = await getBase64(file.originFileObj)
+      }
+      this.previewImage = file.url || file.preview
+      this.previewVisible = true
+    },
+    picHandleChange ({ fileList }) {
+      this.fileList = fileList
+    },
+    imagesInit (images) {
+      if (images !== null && images !== '') {
+        let imageList = []
+        images.split(',').forEach((image, index) => {
+          imageList.push({uid: index, name: image, status: 'done', url: 'http://127.0.0.1:9527/imagesWeb/' + image})
+        })
+        this.fileList = imageList
+      }
+    },
+    setFormValues ({...unit}) {
+      this.rowId = unit.id
+      let fields = ['name']
+      let obj = {}
+      Object.keys(unit).forEach((key) => {
+        if (key === 'images') {
+          this.fileList = []
+          this.imagesInit(unit['images'])
+        }
+        if (fields.indexOf(key) !== -1) {
+          this.form.getFieldDecorator(key)
+          obj[key] = unit[key]
+        }
+      })
+      this.form.setFieldsValue(obj)
+    },
+    reset () {
+      this.loading = false
+      this.form.resetFields()
+    },
+    onClose () {
+      this.reset()
+      this.$emit('close')
+    },
+    handleSubmit () {
+      // 获取图片List
+      let images = []
+      this.fileList.forEach(image => {
+        images.push(image.name)
+      })
+      this.form.validateFields((err, values) => {
+        values.id = this.rowId
+        values.images = images.length > 0 ? images.join(',') : null
+        if (!err) {
+          this.loading = true
+          this.$put('/cos/unit-info', {
+            ...values
+          }).then((r) => {
+            this.reset()
+            this.$emit('success')
+          }).catch(() => {
+            this.loading = false
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
